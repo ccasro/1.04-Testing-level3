f3165b3f7a9728dee3654c34facea66a187ffdd8
Cesar <ccasro@gmail.com>
2025-10-28 18:00:20 +0100

    refactor(test): move Calculator instantiation to beforeEach

--- Testing/src/test/java/org/ccasro/CalculatorTest.java
+++ Testing/src/test/java/org/ccasro/CalculatorTest.java
@@ -1,35 +1,38 @@
 package org.ccasro;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
+    private Calculator calc;
+
+    @BeforeEach
+    void setup() {
+        calc = new Calculator();
+    }
 
     @Test
     void sum(){
-        Calculator calc = new Calculator();
         int result = calc.add(5,10);
         assertThat(result).isEqualTo(15);
     }
 
     @Test
     void sub(){
-        Calculator calc = new Calculator();
         int result = calc.sub(10,100);
         assertThat(result).isEqualTo(-90);
     }
 
     @Test
     void div(){
-        Calculator calc = new Calculator();
         double result = calc.div(50.0,5.0);
         assertThat(result).isEqualTo(10.0);
     }
 
     @Test
     void mul(){
-        Calculator calc = new Calculator();
         int result = calc.mul(10,40);
         assertThat(result).isEqualTo(400);
     }
