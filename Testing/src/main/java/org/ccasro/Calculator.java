9c74c417f1dbdf1ef6e36576970661288a454209
Cesar <ccasro@gmail.com>
2025-10-28 17:43:59 +0100

    feat: implement basic operations to pass tests

--- Testing/src/main/java/org/ccasro/Calculator.java
+++ Testing/src/main/java/org/ccasro/Calculator.java
@@ -2,21 +2,21 @@ package org.ccasro;
 
 public class Calculator {
     public int add(int i, int i1) {
-        return 0;
+        return i + i1;
     }
 
 
     public int sub(int i, int i1) {
-        return 0;
+        return i - i1;
     }
 
 
     public double div(double v, double v1) {
-        return 0;
+        return v / v1;
     }
 
 
     public int mul(int i, int i1) {
-        return 0;
+        return i * i1;
     }
 }
