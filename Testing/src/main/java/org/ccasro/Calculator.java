b00a336de4fd594e6c7b2f4b706f478fe9106248
Cesar <ccasro@gmail.com>
2025-10-28 19:48:50 +0100

    feat(calculator): add overflow checks in add, sub, mul, div

--- Testing/src/main/java/org/ccasro/Calculator.java
+++ Testing/src/main/java/org/ccasro/Calculator.java
@@ -3,19 +3,35 @@ package org.ccasro;
 public class Calculator {
 
     public double div(double a, double b) {
-        return a / b;
+        double result = a / b;
+        if(Double.isInfinite(result) && b != 0.0) {
+            throw new ArithmeticException("Overflow in division");
+        }
+        return result;
     }
 
     public double add(double a, double b) {
-        return a + b;
+        double result = a + b;
+        if(Double.isInfinite(result)) {
+            throw new ArithmeticException("Overflow in addition");
+        }
+        return result;
     }
 
     public double sub(double a, double b) {
-        return a - b;
+        double result = a - b;
+        if(Double.isInfinite(result)) {
+            throw new ArithmeticException("Overflow in substraction");
+        }
+        return result;
     }
 
     public double mul(double a, double b) {
-        return a * b;
+        double result = a * b;
+        if(Double.isInfinite(result)) {
+            throw new ArithmeticException("Overflow in multiplication");
+        }
+        return result;
     }
 
 }
