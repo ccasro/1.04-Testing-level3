acf72554dd5f9f81a41673ed5eb403d9077949b4
Cesar <ccasro@gmail.com>
2025-10-28 18:40:45 +0100

    refactor(calculator): remove int-based methods after migrating to double

--- Testing/src/main/java/org/ccasro/Calculator.java
+++ Testing/src/main/java/org/ccasro/Calculator.java
@@ -1,29 +1,14 @@
 package org.ccasro;
 
 public class Calculator {
-    public int add(int i, int i1) {
-        return i + i1;
-    }
-
-
-    public int sub(int i, int i1) {
-        return i - i1;
-    }
-
 
     public double div(double v, double v1) {
         return v / v1;
     }
 
-
-    public int mul(int i, int i1) {
-        return i * i1;
-    }
-
     public double add(double v, double v1) {
         return v + v1;
     }
-
 
     public double sub(double v, double v1) {
         return v - v1;
