9a47521fcea724b388e804ae7cd70c0364bc3c37
Cesar <ccasro@gmail.com>
2025-10-28 17:37:46 +0100

    test: add failing tests for basic calculator operations

---
+++ Testing/src/main/java/org/ccasro/Calculator.java
@@ -0,0 +1,22 @@
+package org.ccasro;
+
+public class Calculator {
+    public int add(int i, int i1) {
+        return 0;
+    }
+
+
+    public int sub(int i, int i1) {
+        return 0;
+    }
+
+
+    public double div(double v, double v1) {
+        return 0;
+    }
+
+
+    public int mul(int i, int i1) {
+        return 0;
+    }
+}
