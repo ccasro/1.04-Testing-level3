c165d03bbe56cee3c341bcbce7a1cbecc6e3b543
Cesar <ccasro@gmail.com>
2025-10-28 19:25:38 +0100

    refactor(calculator): add more descriptive variable names

--- Testing/src/main/java/org/ccasro/Calculator.java
+++ Testing/src/main/java/org/ccasro/Calculator.java
@@ -2,20 +2,20 @@ package org.ccasro;
 
 public class Calculator {
 
-    public double div(double v, double v1) {
-        return v / v1;
+    public double div(double a, double b) {
+        return a / b;
     }
 
-    public double add(double v, double v1) {
-        return v + v1;
+    public double add(double a, double b) {
+        return a + b;
     }
 
-    public double sub(double v, double v1) {
-        return v - v1;
+    public double sub(double a, double b) {
+        return a - b;
     }
 
-    public double mul(double v, double v1) {
-        return v * v1;
+    public double mul(double a, double b) {
+        return a * b;
     }
 
 }
